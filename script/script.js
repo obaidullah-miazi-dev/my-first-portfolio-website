b9eb825a58c251fff61b585e9a8a4e323ab40b02
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-01 12:42:45 +0600

    little js functionality added

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact').addEventListener('click',function(e){
+    e.preventDefault()
+    document.getElementById('footer-section').scrollIntoView({behavior:'smooth'})
+})
